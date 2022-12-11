5dd6f1f920085143d996f3ae574538baec17abe9
1liornatan <1liornatan@gmail.com>
2022-12-11 13:53:45 +0200

    Operations Enum Fix

--- kaplat/src/main/java/com/mta/kaplat/controller/OperateController.java
+++ kaplat/src/main/java/com/mta/kaplat/controller/OperateController.java
@@ -18,37 +18,38 @@ public class OperateController {
 
     @GetMapping("/stack/operate")
     public ResponseEntity<ResponseModel> operate(String operation) {
-        Operations opr = Operations.valueOf(operation.toUpperCase());
-
-        int size = calculator.size();
-        switch (opr) {
-            case PLUS, MINUS, TIMES, POW, DIVIDE -> {
-                if (size < 2) {
-                    return ResponseEntity.status(HttpStatus.CONFLICT)
-                            .body(new ResponseModel(-1,
-                                    String.format("Error: cannot implement operation %s. It requires 2 arguments and the stack has only %d arguments",
-                                            operation, size)));
-                }
-            }
-            case FACT, ABS -> {
-                if (size < 1) {
-                    return ResponseEntity.status(HttpStatus.CONFLICT)
-                            .body(new ResponseModel(-1, String.format("Error: cannot implement operation %s. It requires 1 arguments and the stack has only %d arguments",
-                                    operation, size)));
-                }
-            }
-            default -> {
-                // ERROR NO OPERATION
-                return ResponseEntity.status(HttpStatus.CONFLICT)
-                        .body(new ResponseModel(-1, Constants.ERROR_NO_OPERATION + operation));
-            }
-        }
-
         try {
-            Integer result = calculator.makeOperation(opr);
-            return ResponseEntity.ok(new ResponseModel(result, ""));
-        } catch (RuntimeException e) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).body(new ResponseModel(-1, e.getMessage()));
+            Operations opr = Operations.valueOf(operation.toUpperCase());
+
+            int size = calculator.size();
+            switch (opr) {
+                case PLUS, MINUS, TIMES, POW, DIVIDE -> {
+                    if (size < 2) {
+                        return ResponseEntity.status(HttpStatus.CONFLICT)
+                                .body(new ResponseModel(-1,
+                                        String.format("Error: cannot implement operation %s. It requires 2 arguments and the stack has only %d arguments",
+                                                operation, size)));
+                    }
+                }
+                case FACT, ABS -> {
+                    if (size < 1) {
+                        return ResponseEntity.status(HttpStatus.CONFLICT)
+                                .body(new ResponseModel(-1, String.format("Error: cannot implement operation %s. It requires 1 arguments and the stack has only %d arguments",
+                                        operation, size)));
+                    }
+                }
+            }
+
+            try {
+                Integer result = calculator.makeOperation(opr);
+                return ResponseEntity.ok(new ResponseModel(result, ""));
+            } catch (RuntimeException e) {
+                return ResponseEntity.status(HttpStatus.CONFLICT).body(new ResponseModel(-1, e.getMessage()));
+            }
+        } catch (IllegalArgumentException e) {
+            // ERROR NO OPERATION
+            return ResponseEntity.status(HttpStatus.CONFLICT)
+                    .body(new ResponseModel(-1, Constants.ERROR_NO_OPERATION + operation));
         }
     }
 }
